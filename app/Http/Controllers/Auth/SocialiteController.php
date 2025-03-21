5e8b6f68d847bfa243723005afa54a535b01459b
Khisrav <kh.khisrav2018@gmail.com>
2025-03-21 16:40:21 +0300

    logout route fixed

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -58,13 +58,13 @@ class SocialiteController extends Controller
         return $user;
     }
 
-    public function logout(Request $request)
-    {
-        Auth::logout();
+    // public function logout(Request $request)
+    // {
+    //     Auth::logout();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+    //     $request->session()->invalidate();
+    //     $request->session()->regenerateToken();
 
-        return redirect('/');
-    }
+    //     return redirect('/');
+    // }
 }

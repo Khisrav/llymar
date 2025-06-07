ee922e1ef8c27dde35b6046bc9a36d0f87735060
Khisrav <kh.khisrav2018@gmail.com>
2025-06-07 12:35:11 +0500

    Enhance role and permission management by adding customizable options and refining guard_name filters; update seeders for consistency.

---
+++ database/seeders/CustomPermissionSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'access admin panel',
+            'access app calculator',
+            'access app history',
+            'access app cart',
+            'access app wholesale-factors',
+            'can access dxf'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}

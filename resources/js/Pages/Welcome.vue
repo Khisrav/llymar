e5550e35771a26f54570ecb8a11cc87eb7875127
Khisrav <kh.khisrav2018@gmail.com>
2025-08-05 23:00:58 +0500

    Refactor GuestHeaderLayout and Welcome page for improved UI and functionality
    - Updated mobile navigation in GuestHeaderLayout with enhanced transition effects and a new menu structure.
    - Changed contact number in the mobile menu for accuracy.
    - Improved styling and animation for menu items to enhance user experience.
    - Updated Welcome page heading for better branding and adjusted button text for consistency in font usage.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,11 +17,11 @@ import { Head, Link } from '@inertiajs/vue3';
             <div class="container max-w-screen-2xl px-4 flex-1 flex flex-col justify-center">
                 <div class="flex flex-col gap-6 md:gap-8 py-12 md:py-0">
                     <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl uppercase font-light leading-tight">
-                        Уникальные алюминиевые системы безрамного остекления
+                        Премиальные алюминиевые системы безрамного остекления
                     </h1>
                     <div class="mt-4">
                         <button class="bg-light-gold hover:bg-light-gold/90 transition-colors flex items-center flex-row gap-3 md:gap-4 text-black py-3 md:py-4 px-5 md:px-6 font-medium rounded-full text-sm md:text-base w-full sm:w-auto justify-center sm:justify-start">
-                            <span>Получить консультацию</span>
+                            <span class="montserrat">Получить консультацию</span>
                             <img src="/assets/arrow.svg" alt="">
                         </button>
                     </div>
